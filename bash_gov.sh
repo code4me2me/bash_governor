4aeccc5faccf5c035efca9ee2c99a87eb2945772
code4me2me <32439114+code4me2me@users.noreply.github.com>
2020-04-30 13:49:50 -0500

    Removed old comments, added indentation

--- bash_gov.sh
+++ bash_gov.sh
@@ -1,25 +1,18 @@
 #!/bin/bash
 PS3="> "
-#mapfile -d\   -t govs < /sys/devices/system/cpu/cpu0/cpufreq/scaling_available_governors
-#cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_available_governors | while read -a govs; do 
 menu_from_array ()
 {
-
-select item; do
-# Check the selected menu item number
-if [ 1 -le "$REPLY" ] && [ "$REPLY" -le $# ];
-
-then
-#echo "The selected operating system is $item"
-echo "$item" | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
-break;
-else
-echo "Wrong selection: Select any number from 1-$#"
-fi
-done
+  select item; do
+  # Check the selected menu item number
+  if [ 1 -le "$REPLY" ] && [ "$REPLY" -le $# ];
+    then
+      echo "$item" | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
+      break;
+    else
+      echo "Wrong selection: Select any number from 1-$#"
+    fi
+  done
 }
 
 read -d "" -a govs < /sys/devices/system/cpu/cpu0/cpufreq/scaling_available_governors
 menu_from_array "${govs[@]}"
-#select  opcion in Performance Powersave Wallpaper Update
-
